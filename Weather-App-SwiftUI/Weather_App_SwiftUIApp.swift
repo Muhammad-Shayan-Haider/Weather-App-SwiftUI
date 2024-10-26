d28ccfe78aab6915d25ff405ef00749cc5727148
mshayan-experlabs <138456600+mshayan-experlabs@users.noreply.github.com>
2024-10-26 10:58:42 +0500

    Initial Commit

---
+++ Weather-App-SwiftUI/Weather_App_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_App_SwiftUIApp.swift
+//  Weather-App-SwiftUI
+//
+//  Created by Muhammad Shayan on 26/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Weather_App_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Weather-App-SwiftUITests/Weather_App_SwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_App_SwiftUITests.swift
+//  Weather-App-SwiftUITests
+//
+//  Created by Muhammad Shayan on 26/10/2024.
+//
+
+import Testing
+@testable import Weather_App_SwiftUI
+
+struct Weather_App_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
